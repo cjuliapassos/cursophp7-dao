949006369c242a45ba99bebd0f1af7ff4e3c9831
cjuliapassos <camillespaos@gmail.com>
2020-09-17 16:54:26 -0400

    adicionado classe usuario

--- index.php
+++ index.php
@@ -2,9 +2,10 @@
 
 require_once("config.php");
 
-$sql = new Sql();
 
-$usuarios = $sql->select("SELECT * FROM tb_usuarios");
+$root = new Usuario();
+$root->loadById(3);
 
-echo json_encode($usuarios);
+echo $root;
+
 ?>
