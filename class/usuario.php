08979f833a8574673735f8d8b93a246647e54f9d
cjuliapassos <camillespaos@gmail.com>
2020-09-18 14:21:43 -0400

    adicionado classe usuario

--- class/usuario.php
+++ class/usuario.php
@@ -1,5 +1,7 @@
 <?php
 
+
+//DAO SELECT
 class Usuario {
 
     private $idusuario;
@@ -38,7 +40,7 @@ class Usuario {
 
     public function loadById($id){
         $sql=new Sql();
-
+//DAO SELECT
         $results = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario= :ID", array(
             ":ID"=>$id
         ));
@@ -51,6 +53,45 @@ class Usuario {
             $this->setDtcadastro(new DateTime($row['dtcadastro']));
         }
     }
+
+
+    //DAO - LIST 
+    public static function getList(){
+        $sql = new Sql();
+        
+       return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin");
+
+    }
+
+    public static function search($login){
+        $sql = new Sql();
+
+        return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+            ':SEARCH'=>"%".$login."%"
+        ));
+    }
+
+    public function login($login, $password){
+        $sql=new Sql();
+        //DAO SELECT
+                $results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha= :PASSWORD", array(
+                    ":LOGIN"=>$login,
+                    ":PASSWORD"=>$password
+                ));
+        
+                if(count($results)>0){
+                    $row = $results[0];
+                    $this->setIdusuario($row['idusuario']);
+                    $this->setDeslogin($row['deslogin']);
+                    $this->setDessenha($row['dessenha']);
+                    $this->setDtcadastro(new DateTime($row['dtcadastro']));
+
+                } else {
+                    throw new Exception("Login e/ou senha inválidos");
+                    
+                }
+    }
+
 
     public function __toString(){
         return json_encode(array(
--- index.php
+++ index.php
@@ -2,10 +2,38 @@
 
 require_once("config.php");
 
-
+///////////////CARREGA UM USUARIO///////////
+/*
 $root = new Usuario();
 $root->loadById(3);
 
 echo $root;
 
-?>
+*/
+
+///////////////CARREGA UMA LISTA DE USUARIOS///////////
+
+/* 
+$lista = Usuario::getList();
+
+echo json_encode($lista);
+
+*/
+
+
+
+///////////////CARREGA UM USUARIO BUSCANDO PELO LOGIN///////////
+/* 
+ $busca = Usuario::search("julia");
+
+ echo json_encode($busca);
+ */
+
+///////////////CARREGA UM USUARIO USANDO O LOGIN E A SENHA///////////
+$usuario = new Usuario();
+
+$usuario->login("julia", "121415");
+
+echo $usuario
+
+?> 
